60b8ed631ce2c6d9c7cead36fc18354c5863057c
aghylassai <aghilassai@gmail.com>
2021-04-24 14:10:14 +0200

    first implementation of HashingTF, IDF,Tokenizer,Word2Vec

--- src/main/scala/io/github/jsarni/PipelineParser/DatasetStage/Word2Vec.scala
+++ src/main/scala/io/github/jsarni/PipelineParser/DatasetStage/Word2Vec.scala
@@ -1,3 +1,11 @@
 package io.github.jsarni.PipelineParser.DatasetStage
+import org.apache.spark.ml.feature.{Word2Vec=> WordtoVec}
+import org.apache.spark.ml.linalg.Vector
+case class Word2Vec(InputCol: String,OutputCol:String, VectorSize:Int,MinCount:Int) extends DatasetStage {
+    val wordtovec=new WordtoVec()
+                      .setInputCol(InputCol)
+                      .setOutputCol(OutputCol)
+                      .setVectorSize(VectorSize)
+                      .setMinCount(MinCount)
+}
 
-case class Word2Vec() extends DatasetStage
