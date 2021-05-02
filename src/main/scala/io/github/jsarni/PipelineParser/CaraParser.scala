d356c252ee693250f9196941fe36da83d9d83394
Juba SARNI <juba.sarni@gmail.com>
2021-05-02 13:24:49 +0200

    Buildin spark ml pipelines and unit tests

--- src/main/scala/io/github/jsarni/PipelineParser/CaraParser.scala
+++ src/main/scala/io/github/jsarni/PipelineParser/CaraParser.scala
@@ -3,6 +3,7 @@ package io.github.jsarni.PipelineParser
 import com.fasterxml.jackson.databind.JsonNode
 import io.github.jsarni.CaraStage.{CaraStage, CaraStageDescription, CaraStageMapper}
 import io.github.jsarni.CaraYaml.CaraYaml
+import org.apache.spark.ml.{Pipeline, PipelineStage}
 
 import scala.collection.JavaConverters._
 import scala.util.Try
@@ -10,6 +11,8 @@ import scala.util.Try
 class CaraParser(caraYaml: CaraYaml) extends ParserUtils with CaraStageMapper{
 
   val content = caraYaml.loadFile()
+
+
 
   private[PipelineParser] def extractStages(fileContent: JsonNode): List[CaraStageDescription]  = {
     val stagesList = fileContent.at(s"/CaraPipeline").iterator().asScala.toList
@@ -39,19 +42,28 @@ class CaraParser(caraYaml: CaraYaml) extends ParserUtils with CaraStageMapper{
     stages
   }
 
-  def parseStage(stageDescription: CaraStageDescription): Try[Any] =
+  private[PipelineParser] def parseStage(stageDescription: CaraStageDescription): Try[Any] =
     for {
       stageClass <- Try(Class.forName(s"io.github.jsarni.CaraStage.ModelStage.${stageDescription.stageName}"))
       constructor <- getMapperConstructor(stageClass)
       caraStage = constructor.newInstance(stageDescription.params)
     } yield caraStage
 
-  def parseStageMap(stageDescription: CaraStageDescription): CaraStage = {
+  private[PipelineParser] def parseStageMap(stageDescription: CaraStageDescription): CaraStage = {
     mapStage(stageDescription)
   }
 
-  def parseAllStages(stagesDescriptionsList: List[CaraStageDescription]): List[CaraStage] = {
+  private[PipelineParser] def parseAllStages(stagesDescriptionsList: List[CaraStageDescription]): List[CaraStage] = {
     stagesDescriptionsList.map(parseStageMap(_))
+  }
+
+  private[PipelineParser] def buildAllStages(stagesList: List[CaraStage]): List[PipelineStage] = {
+    stagesList.map(_.build())
+  }
+
+  private[PipelineParser] def buildPipeline(mlStages: List[PipelineStage]) = {
+    new Pipeline()
+      .setStages(mlStages.toArray)
   }
 
 }
--- src/test/scala/io/github/jsarni/PipelineParser/CaraParserTest.scala
+++ src/test/scala/io/github/jsarni/PipelineParser/CaraParserTest.scala
@@ -1,9 +1,11 @@
 package io.github.jsarni.PipelineParser
 
-import io.github.jsarni.CaraStage.CaraStageDescription
+import io.github.jsarni.CaraStage.{CaraStage, CaraStageDescription}
 import io.github.jsarni.CaraStage.ModelStage.LogisticRegression
 import io.github.jsarni.CaraYaml.CaraYaml
 import io.github.jsarni.TestBase
+import org.apache.spark.ml.{Pipeline, PipelineStage}
+import org.apache.spark.ml.classification.{LogisticRegression => SparkLR}
 
 class CaraParserTest extends TestBase {
 
@@ -14,7 +16,6 @@ class CaraParserTest extends TestBase {
 
     val myJson = caraYaml.loadFile()
 
-
     val extractStages = PrivateMethod[List[CaraStageDescription]]('extractStages)
     val result = caraParser.invokePrivate(extractStages(myJson.get))
 
@@ -23,6 +24,7 @@ class CaraParserTest extends TestBase {
         CaraStageDescription("LogisticRegression", Map("MaxIter" -> "10", "RegParam" -> "0.3", "ElasticNetParam" -> "0.1")),
         CaraStageDescription("FeatureSelection", Map("Param1" -> "S", "Param2" -> "0.5", "Param3" -> "false"))
       )
+
     result should contain theSameElementsAs expectedResult
   }
 
@@ -41,7 +43,7 @@ class CaraParserTest extends TestBase {
 //
 //  }
 
-  "parseStageMap" should "return parse the yaml description file to a json object" in {
+  "parseStageMap" should "parse a CaraStageDescription to a CaraStage " in {
     val caraPath = getClass.getResource("/cara.yaml").getPath
     val caraParser = new CaraParser(CaraYaml(caraPath))
 
@@ -49,13 +51,71 @@ class CaraParserTest extends TestBase {
     val stageDesc =
       CaraStageDescription("LogisticRegression", params)
 
-    val res = caraParser.parseStageMap(stageDesc)
+    val parseStageMap = PrivateMethod[CaraStage]('parseStageMap)
 
+    val res = caraParser.invokePrivate(parseStageMap(stageDesc))
 
     res.isInstanceOf[LogisticRegression] shouldBe true
     res.asInstanceOf[LogisticRegression].MaxIter shouldBe params.get("MaxIter").map(_.toInt)
     res.asInstanceOf[LogisticRegression].RegParam shouldBe params.get("RegParam").map(_.toDouble)
     res.asInstanceOf[LogisticRegression].ElasticNetParam shouldBe params.get("ElasticNetParam").map(_.toDouble)
+  }
 
+  "parseAllStages" should "parse a list of CaraStageDescription to the corresponding list of CaraStage" in {
+    val caraPath = getClass.getResource("/cara.yaml").getPath
+    val caraParser = new CaraParser(CaraYaml(caraPath))
+
+    val params1 = Map("MaxIter" -> "10", "RegParam" -> "0.3", "ElasticNetParam" -> "0.1")
+    val params2 = Map("MaxIter" -> "20", "FitIntercept" -> "False", "ProbabilityCol" -> "col1")
+    val stagesDesc = List(
+      CaraStageDescription("LogisticRegression", params1),
+      CaraStageDescription("LogisticRegression", params2)
+    )
+
+    val expectedResult = List(LogisticRegression(params1), LogisticRegression(params2))
+
+    val parseAllStages = PrivateMethod[List[CaraStage]]('parseAllStages)
+    val res = caraParser.invokePrivate(parseAllStages(stagesDesc))
+
+    res should contain theSameElementsAs expectedResult
+  }
+
+  "buildAllStages" should "build a list PipelineStages out of a list of CaraStages" in {
+    val caraPath = getClass.getResource("/cara.yaml").getPath
+    val caraParser = new CaraParser(CaraYaml(caraPath))
+
+    val params1 = Map("MaxIter" -> "10", "RegParam" -> "0.3", "ElasticNetParam" -> "0.1")
+    val params2 = Map("MaxIter" -> "20", "FitIntercept" -> "False", "ProbabilityCol" -> "col1")
+    val stagesList = List(
+      LogisticRegression(params1), LogisticRegression(params2)
+    )
+
+    val expectedResult = List(
+      new SparkLR().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.1),
+      new SparkLR().setMaxIter(20).setFitIntercept(false).setProbabilityCol("col1")
+    )
+
+    val buildAllStages = PrivateMethod[List[PipelineStage]]('buildAllStages)
+    val res = caraParser.invokePrivate(buildAllStages(stagesList))
+
+    val resParameters = res.map(_.extractParamMap().toSeq.map(_.value))
+    val expectedParameters = expectedResult.map(_.extractParamMap().toSeq.map(_.value))
+
+    resParameters.head should contain theSameElementsAs expectedParameters.head
+    resParameters(1) should contain theSameElementsAs expectedParameters(1)
+  }
+
+  "buildPipeline" should "build a Spark ML Pipeline out of a list of PipelineStages" in {
+    val caraPath = getClass.getResource("/cara.yaml").getPath
+    val caraParser = new CaraParser(CaraYaml(caraPath))
+
+    val stagesList = List(
+      new SparkLR().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.1)
+    )
+
+    val buildPipeline = PrivateMethod[Pipeline]('buildPipeline)
+    val res = caraParser.invokePrivate(buildPipeline(stagesList))
+
+    res.getStages shouldBe new Pipeline().setStages(stagesList.toArray).getStages
   }
 }
