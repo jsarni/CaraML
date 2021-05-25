396840517033229b36b38da0efb394f2e8607cb9
SAI-Aghylas <55828644+SAI-Aghylas@users.noreply.github.com>
2021-05-25 17:55:50 +0200

    Feature/dataset parser (#6)
    * first implementation of HashingTF, IDF,Tokenizer,Word2Vec
    
    * add new Dataset Features + Fix build function
    
    * fix CountVectorizerModel + handle Model classes
    
    * build edited, in progress for
    
    * add tests for all classes -- must review CountVModel to fix tests
    
    * fixed CountVectorizerModel Test
    
    * getMethode completed + all class and tests ok + indentation ok
    
    * fixed PR changes

---
+++ src/main/scala/io/github/jsarni/CaraStage/DatasetStage/CountVectorizerModel.scala
@@ -0,0 +1,55 @@
+package io.github.jsarni.CaraStage.DatasetStage
+
+import io.github.jsarni.CaraStage.Annotation.MapperConstructor
+import org.apache.spark.ml.PipelineStage
+import org.apache.spark.ml.feature.{CountVectorizerModel => fromSparkML}
+
+import scala.util.Try
+
+case class CountVectorizerModel(Binary:Option[Boolean],
+                                InputCol:Option[String],
+                                MaxDF:Option[Double],
+                                MinDF:Option[Double],
+                                MinTF:Option[Double],
+                                OutputCol: Option[String],
+                                VocabSize: Option[Int],
+                                Vocabulary: Option[Array[String]]
+                               ) extends CaraDataset {
+  @MapperConstructor
+  def this(params: Map[String, String]) = {
+    this(params.get("Binary").map(_.toBoolean),
+      params.get("InputCol"),
+      params.get("MaxDF").map(_.toDouble),
+      params.get("MinDF").map(_.toDouble),
+      params.get("MinTF").map(_.toDouble),
+      params.get("OutputCol"),
+      params.get("VocabSize").map(_.toInt),
+      params.get("Vocabulary").map(_.split(','))
+    )
+  }
+
+  @Override
+  def build(): Try[PipelineStage] = Try{
+
+    val datasetFeature=new fromSparkML(this.Vocabulary.getOrElse(Array.empty[String]))
+    val ParamsToNotSet=List("Vocabulary","MaxDF","MinDF","VocabSize")
+    val definedFields = this.getClass.getDeclaredFields
+                                      .filter(f => f.get(this).asInstanceOf[Option[Any]].isDefined)
+                                      .filter(f=> ! ParamsToNotSet.contains(f.getName) )
+    val names = definedFields.map(f => f.getName)
+    val values = definedFields.map(f => f.get(this))
+    val zipFields = names zip values
+
+    zipFields.map(f=> getMethode(datasetFeature,f._2 match {case Some(s) => s },f._1)
+                      .invoke(datasetFeature,(f._2 match {case Some(value) => value.asInstanceOf[f._2.type ] }))
+                  )
+    datasetFeature
+  }
+}
+
+object CountVectorizerModel {
+  def apply(params: Map[String,String]):CountVectorizerModel= new CountVectorizerModel(params)
+}
+
+
+
