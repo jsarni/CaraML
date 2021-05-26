e9c9a83c1b434ba310368e41282b02f4eae31547
Juba SARNI <juba.sarni@gmail.com>
2021-05-26 13:08:41 +0200

    Created CaraModel Pipeline skeleton for train

--- src/main/scala/io/github/jsarni/CaraModel.scala
+++ src/main/scala/io/github/jsarni/CaraModel.scala
@@ -1,21 +1,39 @@
 package io.github.jsarni
 
-import io.github.jsarni.DatasetLoader.CaraLoader
-import io.github.jsarni.PipelineParser.CaraParser
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.ml.PipelineModel
 import io.github.jsarni.CaraYaml.CaraYamlReader
-import scala.util.{Success, Try}
+import io.github.jsarni.DatasetLoader.CaraLoader
+import io.github.jsarni.PipelineParser.{CaraParser, CaraPipeline}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.{Dataset, SparkSession}
 
-final class CaraModel(yamlPath: String, datasetPath: String, format: String)(implicit spark: SparkSession) {
+import scala.util.Try
+
+final class CaraModel(yamlPath: String, datasetPath: String, format: String, savePath: String)(implicit spark: SparkSession) {
 
   val yaml = CaraYamlReader(yamlPath)
   val parser = CaraParser(yaml)
   val loader = CaraLoader(datasetPath, format)
 
-  final val model = ???
+  def run(): Try[Unit] = for {
+    caraPipeline <- parser.build()
+    sparkPipeline <- generateModel(caraPipeline)
+    dataset <- loader.load()
+    fittedModel <- train(sparkPipeline, dataset)
+    _ <- generateReport(fittedModel)
+    _ <- save(fittedModel)
+  } yield ()
 
-  def train() = ???
+  def generateReport(model: PipelineModel) : Try[Unit] = ???
+
+  private def generateModel(caraPipeline: CaraPipeline): Try[Pipeline] = ???
+
+  private def train(pipeline: Pipeline, dataset: Dataset[_]): Try[PipelineModel] = Try {
+    pipeline.fit(dataset)
+  }
+
+  private def save(model: PipelineModel) : Try[Unit] = Try {
+    model.write.save(savePath)
+  }
 
 
 }
