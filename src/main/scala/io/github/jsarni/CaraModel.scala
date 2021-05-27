8b69ce8bf9605884165bb2adf5396fc411762465
merzouk13 <57535044+merzouk13@users.noreply.github.com>
2021-05-27 15:16:18 +0200

    Feature/cara pipeline model (#10)
    * first commit branch
    
    * finish generateModel method and add CaraModelTest class
    
    * review cara_pipine_model test
    
    Co-authored-by: merzouk <merzoukoumedda@gmail.com>

--- src/main/scala/io/github/jsarni/CaraModel.scala
+++ src/main/scala/io/github/jsarni/CaraModel.scala
@@ -5,6 +5,7 @@ import io.github.jsarni.DatasetLoader.CaraLoader
 import io.github.jsarni.PipelineParser.{CaraParser, CaraPipeline}
 import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.ml.tuning.{CrossValidator, TrainValidationSplit}
 
 import scala.util.Try
 
@@ -25,9 +26,42 @@ final class CaraModel(yamlPath: String, datasetPath: String, format: String, sav
 
   def generateReport(model: PipelineModel) : Try[Unit] = ???
 
-  private def generateModel(caraPipeline: CaraPipeline): Try[Pipeline] = ???
+  
+  private def generateModel(caraPipeline: CaraPipeline) : Try[Pipeline] = Try {
+    val pipeline = caraPipeline.pipeline
+    val evaluator = caraPipeline.evaluator
+    val tuningStage = caraPipeline.tuner.tuningStage
+    val methodeName = "set" + caraPipeline.tuner.paramName
+    val model = tuningStage match  {
+      case "CrossValidator" => {
+        val paramValue = caraPipeline.tuner.paramValue.toInt
+        val crossValidatorModel = new CrossValidator()
+          .setEstimator(pipeline)
+          .setEvaluator(evaluator)
+          .setParallelism(2)
 
-  private def train(pipeline: Pipeline, dataset: Dataset[_]): Try[PipelineModel] = Try {
+        crossValidatorModel.getClass.getMethod(methodeName, paramValue.getClass )
+          .invoke(crossValidatorModel,paramValue.asInstanceOf[java.lang.Integer])
+        
+        new Pipeline().setStages(Array(crossValidatorModel))
+      }
+      case "TrainValidationSplit" => {
+        val paramValue = caraPipeline.tuner.paramValue.toDouble
+        val validationSplitModel = new TrainValidationSplit()
+          .setEstimator(pipeline)
+          .setEvaluator(evaluator)
+          .setParallelism(2)
+
+        validationSplitModel.getClass.getMethod(methodeName, paramValue.getClass )
+          .invoke(validationSplitModel,paramValue.asInstanceOf[java.lang.Double])
+
+        new Pipeline().setStages(Array(validationSplitModel))
+      }
+    }
+    model
+  }
+
+  private def train(pipeline:  Pipeline , dataset: Dataset[_]): Try[PipelineModel] = Try {
     pipeline.fit(dataset)
   }
 
