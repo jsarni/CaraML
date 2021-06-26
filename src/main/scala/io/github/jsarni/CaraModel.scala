0461e14efaff46ec5d51ef17d8b18c41cccb8b19
aghylassai <aghilassai@gmail.com>
2021-06-26 17:10:51 +0200

    generate report into txt file

--- src/main/scala/io/github/jsarni/CaraModel.scala
+++ src/main/scala/io/github/jsarni/CaraModel.scala
@@ -1,5 +1,7 @@
 package io.github.jsarni
 
+
+
 import io.github.jsarni.CaraYaml.CaraYamlReader
 import io.github.jsarni.PipelineParser.{CaraParser, CaraPipeline}
 import org.apache.spark.ml.classification.LogisticRegressionModel
@@ -9,6 +11,9 @@ import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder, TrainValidationSplit}
 
 import scala.util.Try
+import java.io._
+import java.sql.Timestamp
+import java.time.Instant
 
 final class CaraModel(yamlPath: String, dataset: Dataset[_], savePath: String)(implicit spark: SparkSession) {
 
@@ -23,15 +28,67 @@ final class CaraModel(yamlPath: String, dataset: Dataset[_], savePath: String)(i
     _ <- save(fittedModel)
   } yield ()
 
+
+  def writeFile(filename: String, lines: Map[String,Any]): Unit = {
+    val file = new File(filename)
+    val bw = new BufferedWriter(new FileWriter(file))
+    for ((field,value) <- lines) {
+      println(s"$field : $value")
+      bw.write(s"$field ; $value")
+      bw.write("\n")
+    }
+    bw.close()
+  }
   private def generateReport(model: PipelineModel) : Try[Unit] =Try {
     val stagesModel=model.stages.last
-    val summaries  =stagesModel match {
-      case m: LogisticRegressionModel => m.summary
-      case m: LinearRegressionModel   => m.summary
-      case _ => _
-    }
-    summaries
+    val rapportDate: String= Timestamp.from(Instant.now()).toString.replace(" ","-")
+    stagesModel match {
+////      case m: LogisticRegressionModel => Map("Accuracy"                 ->m.summary.accuracy
+//                                             ,"featuresCol"             ->m.summary.featuresCol,
+//                                            "fMeasureByLabel"           ->m.summary.fMeasureByLabel,
+//                                            "falsePositiveRateByLabel"  ->m.summary.falsePositiveRateByLabel,
+//                                            "labelCol"                  ->m.summary.labelCol,
+//                                            "labels"                    ->m.summary.labels,
+//                                            "objectiveHistory"          ->m.summary.objectiveHistory,
+//                                            "probabilityCol"            ->m.summary.probabilityCol,
+//                                            "truePositiveRateByLabel"   ->m.summary.truePositiveRateByLabel,
+//                                            "weightCol"                 ->m.summary.weightCol,
+//                                            "weightedFalsePositiveRate" ->m.summary.weightedFalsePositiveRate,
+//                                            "totalIterations"           ->m.summary.totalIterations,
+//                                            "weightedFMeasure"          ->m.summary.weightedFMeasure
+//                                          )
+//
 
+      case m: LogisticRegressionModel => {
+
+        val modelName : String= m.getClass.getName.split('.').last.replace("Model","")
+        val modelMetrics= Map("Model Name"                    ->modelName,
+                              "Report Date Generation"        -> rapportDate,
+                              "Accuracy"                      -> m.summary.accuracy,
+                              "features Col"                  -> m.summary.featuresCol,
+                              "fMeasure By Label"             -> m.summary.fMeasureByLabel.toList,
+                              "false Positive Rate By Label"  -> m.summary.falsePositiveRateByLabel.toList,
+                              "labelCol"                      -> m.summary.labelCol,
+                              "labels"                        -> m.summary.labels.toList,
+                              "objective History"             -> m.summary.objectiveHistory.toList,
+                              "probability Col"               -> m.summary.probabilityCol,
+                              "true Positive Rate By Label"   -> m.summary.truePositiveRateByLabel.toList,
+                              "total Iterations"              -> m.summary.totalIterations,
+                              "prediction Col"                -> m.summary.predictionCol,
+                              "precision By Label"            -> m.summary.precisionByLabel.toList,
+                              "recall By  Label"              -> m.summary.recallByLabel.toList,
+                              "weighted Recall"               -> m.summary.weightedRecall,
+                              "weighted True Positive Rate"   -> m.summary.weightedTruePositiveRate,
+                              "weight Col"                    -> m.summary.weightCol,
+                              "weighted False Positive Rate"  -> m.summary.weightedFalsePositiveRate,
+                              "weighted FMeasure"             -> m.summary.weightedFMeasure
+                          )
+        writeFile(s"/home/aghylassai/Bureau/Spark/Arichtecture_Distribuees/$modelName$rapportDate.txt",modelMetrics)
+      }
+//      case m: LinearRegressionModel   => Map("objectiveHistory"->m.summary.objectiveHistory)
+//      case m: LinearRegressionModel   => val fields=m.summary.getClass.getDeclaredFields
+
+    }
   }
 
   def evaluate(model: PipelineModel, dataset: Dataset[_]): Dataset[_] = {
--- src/test/scala/io/github/jsarni/CaraModelTest.scala
+++ src/test/scala/io/github/jsarni/CaraModelTest.scala
@@ -1,11 +1,16 @@
 package io.github.jsarni
+import io.github.jsarni.CaraStage.DatasetStage._
+import io.github.jsarni.CaraStage.ModelStage.LogisticRegression
 import io.github.jsarni.CaraStage.TuningStage.TuningStageDescription
 import io.github.jsarni.PipelineParser.CaraPipeline
-import org.apache.spark.ml.Pipeline
+import org.apache.avro.generic.GenericData.StringType
+import org.apache.spark.ml.{Pipeline, classification, feature}
 import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, RegressionEvaluator}
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
 import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.ml.tuning.{CrossValidator, TrainValidationSplit}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
 
 import scala.util.Try
 
@@ -26,7 +31,7 @@ class CaraModelTest extends TestBase {
         .master("local[1]")
         .getOrCreate()
 
-    val caraModel = new CaraModel("YamlPath", spark.emptyDataFrame, "format", "savePath")(spark)
+    val caraModel = new CaraModel("YamlPath", spark.emptyDataFrame, "savePath")(spark)
     val pipeline = new Pipeline()
       .setStages(Array(lr))
     val crossCaraPipeline = CaraPipeline(pipeline, crossEvaluator, crossTuner)
@@ -46,5 +51,43 @@ class CaraModelTest extends TestBase {
     splitModel.get.getStages.head.isInstanceOf[TrainValidationSplit] shouldBe true
     splitModel.get.getStages.head.asInstanceOf[TrainValidationSplit].getTrainRatio shouldBe 0.6
 
+  }
+"generateReport" should "return the Pipeline metrics" in {
+
+  implicit val spark: SparkSession =
+    SparkSession.builder()
+      .appName("CaraML")
+      .master("local[1]")
+      .getOrCreate()
+
+  val Data = spark.createDataFrame(Seq(
+    (0.0, 0.21, 0.66),
+    (0.0, 0.38, 0.78),
+    (1.0, 0.55, 0.25),
+    (1.0, 0.70, 0.10),
+    (1.0, 0.91, 0.06),
+    (0.0, 0.27, 0.70)
+  )).toDF("labels", "values1","values2")
+
+  val cols = Array( "values1","values2")
+  val assembler = new VectorAssembler()
+    .setInputCols(cols)
+    .setOutputCol("features")
+  val featuresDf = assembler.transform(Data)
+
+  val indexer = new StringIndexer()
+    .setInputCol("labels")
+    .setOutputCol("label")
+  val labelsDf = indexer.fit(featuresDf).transform(featuresDf)
+  //labelsDf.show()
+
+  val logi = LogisticRegression(Map("MaxIter"->"10")).build().get
+
+  val caraModel = new CaraModel("YamlPath", labelsDf, "savePath")(spark)
+  val pipeline = new Pipeline()
+    .setStages(Array(logi))
+  val method = PrivateMethod[Try[Unit]]('generateReport)
+  val fited=pipeline.fit(labelsDf)
+  val crossModels = caraModel.invokePrivate(method(fited)).get
   }
 }
