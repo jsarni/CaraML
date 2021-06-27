05e6c6e008cd7166da3ddc8ee281f0d6a43bded5
merzouk13 <57535044+merzouk13@users.noreply.github.com>
2021-06-27 12:44:25 +0200

    Feature/model schema (#15)
    * LogisticRegressionTest contains error to clear
    
    * Finalize LogisticRegression's class and  tests
    
    * refactor names to caml case and correct spaces
    
    * Adjust LogisticRegretion format code and add DecisionTreeClassifier model class's and test's
    
    * Add GBTClassifier model class's and tests
    
    * tests not ended
    
    * finilize tests new models classes
    
    * CarastageMapper update
    
    * update caraMapperModel
    
    * Add Kmeans, LDA and NaiveBayes models and class's tests
    
    Co-authored-by: merzouk <merzoukoumedda@gmail.com>

---
+++ src/main/scala/io/github/jsarni/CaraStage/ModelStage/KMeans.scala
@@ -0,0 +1,46 @@
+package io.github.jsarni.CaraStage.ModelStage
+
+import io.github.jsarni.CaraStage.Annotation.MapperConstructor
+import org.apache.spark.ml.PipelineStage
+import org.apache.spark.ml.clustering.{KMeans => SparkML}
+import scala.util.Try
+
+
+case class KMeans(DistanceMeasure : Option[String], FeaturesCol : Option[String], K : Option[Int], MaxIter : Option[Int],
+                  PredictionCol : Option[String], Seed : Option[Long], Tol : Option[Double], WeightCol : Option[String] )
+
+  extends CaraModel {
+
+  @MapperConstructor
+  def this(params: Map[String, String]) = {
+    this(
+      params.get("DistanceMeasure"),
+      params.get("FeaturesCol"),
+      params.get("K").map(_.toInt),
+      params.get("MaxIter").map(_.toInt),
+      params.get("PredictionCol"),
+      params.get("Seed").map(_.toLong),
+      params.get("Tol").map(_.toDouble),
+      params.get("WeightCol")
+
+    )
+  }
+
+  override def build(): Try[PipelineStage] = Try {
+    val model = new SparkML()
+    val definedFields = this.getClass.getDeclaredFields.filter(f => f.get(this).asInstanceOf[Option[Any]].isDefined)
+    val names = definedFields.map(f => f.getName)
+    val values = definedFields.map(f => f.get(this))
+    val zipFields = names zip values
+    zipFields.map { f =>
+      val fieldName = f._1
+      val fieldValue = f._2 match {case Some(s) => s }
+      getMethode(model,fieldValue,fieldName)
+        .invoke(model,fieldValue.asInstanceOf[f._2.type])
+    }
+    model
+  }
+}
+object KMeans {
+  def apply(params: Map[String, String]): KMeans = new KMeans(params)
+}
---
+++ src/test/scala/io/github/jsarni/CaraStage/ModelStage/KMeansTest.scala
@@ -0,0 +1,69 @@
+package io.github.jsarni.CaraStage.ModelStage
+
+import org.apache.spark.ml.clustering.{KMeans => SparkML}
+import io.github.jsarni.TestBase
+
+class KMeansTest extends TestBase {
+  "build" should "Create an lr model and set all parameters with there args values or set default ones" in {
+    val params = Map(
+      "DistanceMeasure" -> "euclidean",
+      "FeaturesCol" -> "FeaturesCol",
+      "K" -> "5",
+      "MaxIter" -> "12",
+      "PredictionCol" -> "PredictionCol",
+      "Seed" -> "1214151",
+      "Tol" -> "0.2",
+      "WeightCol" -> "WeightColname"
+    )
+
+    val Kmeans = KMeans(params)
+    val KmeansWithTwoParams = new SparkML()
+      .setTol(0.3)
+      .setDistanceMeasure("euclidean")
+
+    val expectedResult = List(
+      new SparkML()
+        .setDistanceMeasure("euclidean")
+        .setFeaturesCol("FeaturesCol")
+        .setK(5)
+        .setMaxIter(12)
+        .setPredictionCol("PredictionCol")
+        .setSeed(1214151)
+        .setTol(0.2)
+        .setWeightCol("WeightColname")
+    )
+    Kmeans.build().isSuccess shouldBe true
+
+    val res = List(Kmeans.build().get)
+    val resParameters = res.map(_.extractParamMap().toSeq.map(_.value))
+    val expectedParameters = expectedResult.map(_.extractParamMap().toSeq.map(_.value))
+
+    resParameters.head should contain theSameElementsAs expectedParameters.head
+
+    //    Test default values of unset params
+    KmeansWithTwoParams.getMaxIter shouldBe 20
+    KmeansWithTwoParams.getK shouldBe 2
+
+  }
+  "GetMethode" should "Return the appropriate methode by it's name" in {
+    val params = Map(
+      "DistanceMeasure" -> "euclidean",
+      "FeaturesCol" -> "FeaturesCol",
+      "K" -> "5",
+      "MaxIter" -> "12",
+      "PredictionCol" -> "PredictionCol",
+      "Seed" -> "1214151",
+      "Tol" -> "0.2",
+      "WeightCol" -> "WeightColname"
+    )
+    val caraKmeans = KMeans(params)
+    val model =caraKmeans.build().get.asInstanceOf[SparkML]
+
+    caraKmeans.getMethode(model,10,"MaxIter").getName shouldBe "setMaxIter"
+    caraKmeans.getMethode(model,2,"K").getName shouldBe "setK"
+    caraKmeans.getMethode(model, "euclidean" ,"DistanceMeasure").getName shouldBe "setDistanceMeasure"
+
+  }
+
+
+}
