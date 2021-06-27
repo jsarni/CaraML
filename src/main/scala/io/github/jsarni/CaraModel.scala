40ceadc0c33275ac2da2a9bca34c6492dfdc3e3e
aghylassai <aghilassai@gmail.com>
2021-06-27 12:51:37 +0200

    add 3 classifiers

--- src/main/scala/io/github/jsarni/CaraModel.scala
+++ src/main/scala/io/github/jsarni/CaraModel.scala
@@ -4,7 +4,7 @@ package io.github.jsarni
 
 import io.github.jsarni.CaraYaml.CaraYamlReader
 import io.github.jsarni.PipelineParser.{CaraParser, CaraPipeline}
-import org.apache.spark.ml.classification.LogisticRegressionModel
+import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, GBTClassificationModel, LogisticRegressionModel, RandomForestClassificationModel}
 import org.apache.spark.ml.regression.LinearRegressionModel
 import org.apache.spark.ml.{Pipeline, PipelineModel, classification}
 import org.apache.spark.sql.{Dataset, SparkSession}
@@ -45,51 +45,106 @@ final class CaraModel(yamlPath: String, dataset: Dataset[_], savePath: String)(i
     val stagesModel=model.stages.last
     val rapportDate: String= Timestamp.from(Instant.now()).toString.replace(" ","-")
     stagesModel match {
-////      case m: LogisticRegressionModel => Map("Accuracy"                 ->m.summary.accuracy
-//                                             ,"featuresCol"             ->m.summary.featuresCol,
-//                                            "fMeasureByLabel"           ->m.summary.fMeasureByLabel,
-//                                            "falsePositiveRateByLabel"  ->m.summary.falsePositiveRateByLabel,
-//                                            "labelCol"                  ->m.summary.labelCol,
-//                                            "labels"                    ->m.summary.labels,
-//                                            "objectiveHistory"          ->m.summary.objectiveHistory,
-//                                            "probabilityCol"            ->m.summary.probabilityCol,
-//                                            "truePositiveRateByLabel"   ->m.summary.truePositiveRateByLabel,
-//                                            "weightCol"                 ->m.summary.weightCol,
-//                                            "weightedFalsePositiveRate" ->m.summary.weightedFalsePositiveRate,
-//                                            "totalIterations"           ->m.summary.totalIterations,
-//                                            "weightedFMeasure"          ->m.summary.weightedFMeasure
-//                                          )
-//
-
-      case m: LogisticRegressionModel => {
+      case m: DecisionTreeClassificationModel => {
 
         val modelName : String= m.getClass.getName.split('.').last.replace("Model","")
-        val modelMetrics= mutable.SortedMap(
-                              "Model Name"                    ->modelName,
-                              "Report Date Generation"        -> rapportDate,
-                              "Accuracy"                      -> m.summary.accuracy,
-                              "features Col"                  -> m.summary.featuresCol,
-                              "fMeasure By Label"             -> m.summary.fMeasureByLabel.toList,
-                              "false Positive Rate By Label"  -> m.summary.falsePositiveRateByLabel.toList,
-                              "labelCol"                      -> m.summary.labelCol,
-                              "labels"                        -> m.summary.labels.toList,
-                              "objective History"             -> m.summary.objectiveHistory.toList,
-                              "probability Col"               -> m.summary.probabilityCol,
-                              "true Positive Rate By Label"   -> m.summary.truePositiveRateByLabel.toList,
-                              "total Iterations"              -> m.summary.totalIterations,
-                              "prediction Col"                -> m.summary.predictionCol,
-                              "precision By Label"            -> m.summary.precisionByLabel.toList,
-                              "recall By  Label"              -> m.summary.recallByLabel.toList,
-                              "weighted Recall"               -> m.summary.weightedRecall,
-                              "weighted True Positive Rate"   -> m.summary.weightedTruePositiveRate,
-                              "weight Col"                    -> m.summary.weightCol,
-                              "weighted False Positive Rate"  -> m.summary.weightedFalsePositiveRate,
-                              "weighted FMeasure"             -> m.summary.weightedFMeasure
-                          )
-        writeFile(s"/home/aghylassai/Bureau/Spark/Arichtecture_Distribuees/$modelName$rapportDate.txt",modelMetrics)
+        val modelMetrics = mutable.SortedMap(
+                              "Model Name"                    -> modelName,
+                              "Raport Date Generation"        -> rapportDate,
+                              "featureImportances"            -> m.featureImportances.toArray.toList,
+                              "numFeatures"                   -> m.numFeatures,
+                              "featuresCol"                   -> m.featuresCol.name)
+        writeFile(savePath+ s"/ModelResults.txt",modelMetrics)
       }
-//      case m: LinearRegressionModel   => Map("objectiveHistory"->m.summary.objectiveHistory)
-//      case m: LinearRegressionModel   => val fields=m.summary.getClass.getDeclaredFields
+      case m: LogisticRegressionModel => {
+        val summ=m.summary
+        val modelName : String= m.getClass.getName.split('.').last.replace("Model","")
+        val modelMetrics= mutable.SortedMap(
+                              "Model Name"                    -> modelName,
+                              "Report Date Generation"        -> rapportDate,
+                              "Accuracy"                      -> summ.accuracy,
+                              "features Col"                  -> summ.featuresCol,
+                              "fMeasure By Label"             -> summ.fMeasureByLabel.toList,
+                              "false Positive Rate By Label"  -> summ.falsePositiveRateByLabel.toList,
+                              "labelCol"                      -> summ.labelCol,
+                              "labels"                        -> summ.labels.toList,
+                              "objective History"             -> summ.objectiveHistory.toList,
+                              "probability Col"               -> summ.probabilityCol,
+                              "true Positive Rate By Label"   -> summ.truePositiveRateByLabel.toList,
+                              "total Iterations"              -> summ.totalIterations,
+                              "prediction Col"                -> summ.predictionCol,
+                              "precision By Label"            -> summ.precisionByLabel.toList,
+                              "recall By  Label"              -> summ.recallByLabel.toList,
+                              "weighted Recall"               -> summ.weightedRecall,
+                              "weighted True Positive Rate"   -> summ.weightedTruePositiveRate,
+                              "weight Col"                    -> summ.weightCol,
+                              "weighted False Positive Rate"  -> summ.weightedFalsePositiveRate,
+                              "weighted FMeasure"             -> summ.weightedFMeasure
+                          )
+        writeFile(savePath+ s"/ModelResults.txt",modelMetrics)
+      }
+      case m: LinearRegressionModel   => {
+
+        val summ = m.summary
+        val modelName : String= m.getClass.getName.split('.').last.replace("Model","")
+        val modelMetrics= mutable.SortedMap(
+                              "Model Name"                    -> modelName,
+                              "Report Date Generation"        -> rapportDate,
+                              "objectiveHistory"              -> summ.objectiveHistory.toList,
+                              "totalIterations"               -> summ.totalIterations,
+                              "coefficientStandardErrors"     -> summ.coefficientStandardErrors.toList,
+                              "degreesOfFreedom"              -> summ.degreesOfFreedom,
+                              "devianceResiduals"             -> summ.devianceResiduals.toList,
+                              "explainedVariance"             -> summ.explainedVariance,
+                              "featuresCol"                   -> summ.featuresCol,
+                              "labelCol"                      -> summ.labelCol,
+                              "meanAbsoluteError"             -> summ.meanAbsoluteError,
+                              "meanSquaredError"              -> summ.meanSquaredError,
+                              "numInstances"                  -> summ.numInstances,
+                              "predictionCol"                 -> summ.predictionCol,
+                              "pValues"                       -> summ.pValues.toList,
+                              "r2"                            -> summ.r2,
+                              "r2adj"                         -> summ.r2adj,
+                              "rootMeanSquaredError"          -> summ.rootMeanSquaredError,
+                              "tValues"                       -> summ.tValues.toList)
+        writeFile(savePath+ s"/ModelResults.txt",modelMetrics)
+      }
+      case m: GBTClassificationModel   => {
+        val modelName : String= m.getClass.getName.split('.').last.replace("Model","")
+        val modelMetrics = mutable.SortedMap(
+          "Model Name"                    -> modelName,
+          "Raport Date Generation"        -> rapportDate,
+          "featureImportances"            -> m.featureImportances.toArray.toList,
+          ""                              -> m.numFeatures,
+          ""                              -> m.featuresCol,
+          ""                              -> m.getNumTrees,
+          ""                              -> m.toString())
+        writeFile(savePath+ s"/ModelResults.txt",modelMetrics)
+
+      }
+      case m:RandomForestClassificationModel => {
+        val summ = m.summary
+        val modelName : String= m.getClass.getName.split('.').last.replace("Model","")
+        val modelMetrics= mutable.SortedMap(
+          "Model Name"                    -> modelName,
+          "Report Date Generation"        -> rapportDate,
+          "accuracy"                      -> summ.accuracy,
+          "weightedTruePositiveRate"      -> summ.weightedTruePositiveRate,
+          "totalIterations"               -> summ.totalIterations,
+          "objectiveHistory"              -> summ.objectiveHistory.toList,
+          "recallByLabel"                 -> summ.recallByLabel.toList,
+          "weightedRecall"                -> summ.weightedRecall,
+          "precisionByLabel"              -> summ.precisionByLabel.toList,
+          "labels"                        -> summ.labels.toList,
+          "labelCol"                      -> summ.labelCol,
+          "predictionCol"                 -> summ.predictionCol,
+          "falsePositiveRateByLabel"      -> summ.falsePositiveRateByLabel.toList,
+          "weightCol"                     -> summ.weightCol,
+          "fMeasureByLabel"               -> summ.fMeasureByLabel.toList,
+          "weightedFMeasure"              -> summ.weightedFMeasure,
+          "weightedPrecision"             -> summ.weightedPrecision
+        )
+      }
 
     }
   }
